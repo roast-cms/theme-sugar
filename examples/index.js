858dbb8efb8089733f45280951bebaa810a967e9
Dmitri <d@eurekaking.com>
2018-03-18 16:54:14 +0700

    Rewrote theme & evnironment Rewrote theme as an object Made theme customization easier (breaking changes) Rewrote environment to allow fonts

--- examples/index.js
+++ examples/index.js
@@ -5,21 +5,28 @@ import styled, { ThemeProvider } from "styled-components"
 
 // theme
 import { Sugar } from "../src/index"
+import "typeface-roboto"
+import "typeface-open-sans"
 
 const Main = styled.div`
+  & > * ::selection {
+    background: ${props => props.theme.color.highlight()};
+    color: ${props=> props.theme.color.background()}
+  }
   ${props => props.theme.size.font.auto} ${props =>
       props.theme.typography.text.auto} margin: 0 auto;
-  max-width: ${props => props.theme.size.block.column.maxwidth.m}px;
+  max-width: ${props => props.theme.size.block.column.m}px;
   ${props =>
     props.theme.size.breakpoint.min.xxl`max-width: ${props =>
-      props.theme.size.block.column.maxwidth.l}px;`};
-  padding: 0 ${props => props.theme.size.block.column.safety}em;
+      props.theme.size.block.column.l}px;`};
+  padding: 0 ${props => props.theme.size.block.padding}em;
 `
 const Article = styled.article`
+  background: ${props => props.theme.color.background()};
   padding: ${props => props.theme.size.block.spacing}em;
   border-radius: ${props => props.theme.effects.borderRadius.med}em;
   border: ${props => props.theme.size.block.border}px solid
-    ${props => props.theme.color.highlight};
+    ${props => props.theme.color.highlight()};
 `
 const Title = styled.h1`
   ${props => props.theme.typography.title.auto} font-size: ${props =>
@@ -30,20 +37,38 @@ const Subtitle = styled.h2`
   ${props => props.theme.typography.title.auto} font-size: ${props =>
       props.theme.size.font.make.larger / 2}em;
 `
-const Branded = styled.strong`color: ${props => props.theme.color.brand};`
+const Branded = styled.strong`color: ${props => props.theme.color.brand()};`
 const BrandedFade = styled(Branded)`
-  color: ${props => props.theme.color.alpha.brand(props.theme.opacity.half)}
+  color: ${props => props.theme.color.brand(props.theme.opacity.half)};
+  font-size: ${props => props.theme.size.font.make.smaller}em;
 `
 
 render(
   <div>
-    <ThemeProvider theme={Sugar}>
+    <ThemeProvider
+      theme={Sugar({
+        color_brand: "rgb(255,144,244)",
+        color_background: "rgb(44,44,44)",
+        color_foreground: "rgb(224,213,255)",
+
+        font_body: "Roboto",
+        size_base: 30,
+        size_column_medium: 900,
+        size_column_large: 1200,
+        size_block_padding: 2,
+        size_block_spacing: 1.5,
+        size_block_border: 10,
+
+        effects_border_radius: 0
+      })}
+    >
       <Main>
         <Article>
           <Title>Title</Title>
           <Subtitle>Subtitle</Subtitle>
           <p>
-            Paragraph text. <Branded>Brand colour</Branded>, <BrandedFade>faded brand clour</BrandedFade>.
+            Paragraph text. <Branded>Brand colour</Branded>,{" "}
+            <BrandedFade>faded brand clour</BrandedFade>.
           </p>
         </Article>
       </Main>
--- src/index.js
+++ src/index.js
@@ -1,124 +1,163 @@
+//
+// tools and utilities
 import { css } from "styled-components"
 import { exact, min, max, breakpoints } from "./utils"
+//
+// theme object
+export const Sugar = (options = {}) => {
+  //
+  // function to convert rgb value to rgba
+  const rgba = (rgb, a = 1) =>
+    `${rgb.replace(")", "").replace("rgb", "rgba")}, ${a})`
+  //
+  // theme defaults
+  const palette = {
+    //
+    // colours
+    color_brand: options.color_brand || "rgb(0,167,255)",
+    color_foreground: options.color_foreground || "rgb(82,82,82)",
+    color_background: options.color_background || "rgb(255,255,255)",
+    color_highlight: options.color_highlight || "rgb(255,255,0)",
+    //
+    // fonts
+    font_heading_system: options.font_heading_system || "Arial, sans-serif",
+    get font_heading_load() {
+      return options.font_heading_load || this.font_heading_system
+    },
+    get font_body_system() {
+      return options.font_body_system || this.font_heading_system
+    },
+    get font_body_load() {
+      return options.font_body_load || this.font_body_system
+    },
+    //
+    // size
+    size_base: options.size_base || 18,
+    size_column_medium: options.size_column_medium || 700,
+    size_column_large: options.size_column_large || 900,
+    size_block_spacing: options.size_block_spacing || 0.75,
+    size_block_padding: options.size_block_padding || 1.5,
+    size_block_border: options.size_block_border || 4,
 
-export const Sugar = {
-  color: {
-    brand: "#00a7ff",
-    foreground: "#525252",
-    background: "#fff",
-    highlight: "#8cff00",
-    alpha: {
-      foreground: alpha => {
-        return `rgba(82, 82, 82, ${alpha})`
-      },
-      brand: alpha => {
-        return `rgba(0, 167, 255, ${alpha})`
-      },
-      highlight: alpha => {
-        return `rgba(140, 255, 0, ${alpha})`
-      }
-    }
-  },
-  opacity: {
-    most: 0.85,
-    half: 0.5,
-    least: 0.125
-  },
-  effects: {
-    borderRadius: {
-      // base: multiplier
-      small: 0.5,
-      med: 1
-    }
-  },
-  typography: {
-    font: {
-      sans: "Arial, sans-serif",
-      serif: "Georgia, serif"
+    effects_border_radius:
+      options.effects_border_radius !== undefined
+        ? options.effects_border_radius
+        : 0.45
+  }
+  //
+  // return theme defaults object
+  return {
+    color: {
+      brand: alpha => rgba(palette.color_brand, alpha),
+      foreground: alpha => rgba(palette.color_foreground, alpha),
+      background: alpha => rgba(palette.color_background, alpha),
+      highlight: alpha => rgba(palette.color_highlight, alpha)
     },
-    title: {
-      auto: css`
-        font-family: Arial, sans-serif;
-        letter-spacing: 0.005em;
-        line-height: ${() => Sugar.typography.title.lineHeight}em;
-        font-weight: 700;
-        margin: 0;
-        /* in some cases this vv doesn't work and causes garbage CSS */
-        .fonts-loaded-headers & {
-          ${() => Sugar.typography.title.fontsLoaded};
-        }
+    typography: {
+      font: {
+        heading: palette.font_heading,
+        body: palette.font_body
+      },
+      title: {
+        auto: css`
+          color: ${() => Sugar(palette).color.foreground()};
+          font-family: ${() => Sugar(palette).typography.font.heading.system};
+          letter-spacing: 0.005em;
+          line-height: ${() => Sugar(palette).typography.title.lineHeight}em;
+          font-weight: 700;
+          margin: 0;
+          .fonts-loaded-headers & {
+            ${() => Sugar(palette).typography.title.fontsLoaded};
+          }
+        `,
+        fontsLoaded: css`
+          font-family: ${() =>
+            Sugar(palette).typography.font.heading.load}, ${() =>
+          Sugar(palette).typography.font.heading.system};
+          letter-spacing: 0.025em;
+          font-weight: 600;
       `,
-      fontsLoaded: css`
-        font-family: ${() => Sugar.typography.font.sans};
+        lineHeight: 1.15
+      },
+      text: {
+        auto: css`
+          color: ${() => Sugar(palette).color.foreground()};
+          font-family: ${() => Sugar(palette).typography.font.body.system};
+          letter-spacing: 0.05em;
+          line-height: ${() => Sugar(palette).typography.text.lineHeight}em;
+          .fonts-loaded & {
+            ${() => Sugar(palette).typography.text.fontsLoaded};
+          }
+        `,
+        fontsLoaded: css`
+        font-family: ${() => Sugar(palette).typography.font.body.load}, ${() =>
+          Sugar(palette).typography.font.body.system};
+          letter-spacing: 0.025em;
       `,
-      lineHeight: 1.15 // base: multiplier
-    },
-    text: {
-      auto: css`
-        font-family: Georgia, serif;
-        letter-spacing: 0.05em;
-        line-height: ${() => Sugar.typography.text.lineHeight}em;
-        .fonts-loaded & {
-          ${() => Sugar.typography.text.fontsLoaded};
-        }
-      `,
-      fontsLoaded: css`
-        font-family: ${() => Sugar.typography.font.serif};
-      `,
-      lineHeight: 1.75 // base: multiplier
-    }
-  },
-  size: {
-    breakpoint: {
-      exact,
-      min,
-      max,
-      stops: {
-        min: breakpoints.xs[1],
-        max: breakpoints.xl[1]
+        lineHeight: 1.75
       }
     },
-    font: {
-      // base: pixels
-      l: 20,
-      m: 18,
-      s: 14,
-      xs: 12,
-      make: {
-        // base: multiplier
-        larger: 2,
-        normal: 1,
-        smaller: 0.85,
-        tiny: 0.5
-      },
-      // automatically set font size based on screen size; should be at the top of most components' css
-      auto: css`
-        ${min.m`font-size: 	${() =>
-          Sugar.size.font.m}px;`} ${max.s`font-size: 	${() =>
-        Sugar.size.font.s}px;`} ${max.xs`font-size: 	${() =>
-        Sugar.size.font.xs}px;`} ${min.xxl`font-size:	${() =>
-        Sugar.size.font.l}px;`};
-      `
+    effects: {
+      borderRadius: {
+        med: palette.effects_border_radius,
+        small: () => Sugar(palette).effects.borderRadius.med / 2
+      }
     },
-    block: {
-      column: {
-        maxwidth: {
-          // base: pixels
-          m: 700,
-          l: 900
+    size: {
+      breakpoint: {
+        exact,
+        min,
+        max,
+        stops: {
+          min: breakpoints.xs[1],
+          max: breakpoints.xl[1]
+        }
+      },
+      font: {
+        m: palette.size_base,
+        //
+        l: palette.size_base * 1.15,
+        s: palette.size_base * 0.9,
+        xs: palette.size_base * 0.85,
+        //
+        make: {
+          larger: 2,
+          normal: 1,
+          smaller: 0.85,
+          tiny: 0.5
         },
-        safety: 1.5 // base: multiplier
+        auto: css`
+        ${min.m`font-size: 	${() =>
+          Sugar(palette).size.font.m}px;`} ${max.s`font-size: 	${() =>
+          Sugar(palette).size.font.s}px;`} ${max.xs`font-size: 	${() =>
+          Sugar(palette).size.font.xs}px;`} ${min.xxl`font-size:	${() =>
+          Sugar(palette).size.font.l}px;`};
+      `
       },
-      spacing: 0.75, // base: multiplier
-      border: 4, // base: pixels
-      minFigureWIdth: 320 // base: pixels
+      block: {
+        column: {
+          m: palette.size_column_medium,
+          l: palette.size_column_large
+        },
+        padding: palette.size_block_padding,
+        spacing: palette.size_block_spacing,
+        border: palette.size_block_border
+      }
+    },
+    layer: {
+      overlay: 40,
+      card: 30,
+      nav: 20,
+      up: 10,
+      tuck: -1
+    },
+    opacity: {
+      most: 0.85,
+      half: 0.5,
+      least: 0.125
     }
-  },
-  layer: {
-    overlay: 40,
-    card: 30,
-    nav: 20,
-    up: 10,
-    tuck: -1
   }
 }
+//
+// convenience exports
+export { exact, min, max, breakpoints } from "./utils"
