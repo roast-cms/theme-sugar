82d1e67ec92604872709e0c983eb7a0c6f60216f
dmitrizzle <d@artsocket.com>
2018-10-04 11:41:15 -0700

    Flexible sizes

--- examples/index.js
+++ examples/index.js
@@ -1,53 +1,64 @@
 //
 // tools
-import React from "react"
-import { render } from "react-dom"
-import styled, { ThemeProvider } from "styled-components"
+import React from "react";
+import { render } from "react-dom";
+import styled, { ThemeProvider } from "styled-components";
 //
 // theming tools and fonts
-import { Sugar } from "../src/index"
-import "typeface-yanone-kaffeesatz"
-import "typeface-lobster-two"
-import "typeface-indie-flower"
+import { Sugar } from "../src/index";
+import "typeface-yanone-kaffeesatz";
+import "typeface-lobster-two";
+import "typeface-indie-flower";
 //
 // styled components
+//
+//
+
+import { S } from "../src/new";
+console.log(S().size("m"));
+//
+//
+//
+//
 const Main = styled.div`
   & > * ::selection {
     background: ${props => props.theme.color.highlight()};
     color: ${props => props.theme.color.background()};
   }
   ${props => props.theme.size.font.auto} ${props =>
-      props.theme.typography.text.auto} margin: 0 auto;
+    props.theme.typography.text.auto} margin: 0 auto;
   max-width: ${props => props.theme.size.block.column.m}px;
   ${props =>
     props.theme.size.breakpoint.min.xxl`max-width: ${props =>
       props.theme.size.block.column.l}px;`};
   padding: 4em ${props => props.theme.size.block.padding}em;
-`
+`;
 const Article = styled.article`
   background: ${props => props.theme.color.background()};
   padding: ${props => props.theme.size.block.spacing}em;
   border-radius: ${props => props.theme.effects.borderRadius.med}em;
   border: ${props => props.theme.size.block.border}px solid
     ${props => props.theme.color.highlight()};
-`
+`;
 const Title = styled.h1`
   ${props => props.theme.typography.title.auto} font-size: ${props =>
-      props.theme.size.font.make.larger}em;
+  props.theme.size.font.make.larger}em;
   hyphens: auto;
-`
+`;
 const Subtitle = styled.h2`
   ${props => props.theme.typography.title.auto} font-size: ${props =>
-      props.theme.size.font.make.larger / 2}em;
-`
-const Branded = styled.strong`color: ${props => props.theme.color.brand()};`
+  props.theme.size.font.make.larger / 2}em;
+`;
+const Branded = styled.strong`
+  color: ${props => props.theme.color.brand()};
+`;
 const BrandedFade = styled(Branded)`
   color: ${props => props.theme.color.brand(props.theme.opacity.half)};
   font-size: ${props => props.theme.size.font.make.smaller}em;
-`
+`;
 const SpecialFont = styled.span`
   font-family: ${props => props.theme.font_special};
-`
+`;
 //
 // app
 render(
@@ -90,4 +101,4 @@ render(
     </ThemeProvider>
   </div>,
   window.document.getElementById("app")
-)
+);
